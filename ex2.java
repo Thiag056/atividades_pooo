b71cc582cc79475684ee6cb1dbee779c993bbf98
Thiag056 <thiagofranciscosantosribeiro@gmail.com>
2025-08-21 20:52:11 -0300

    Add files via upload

--- ex2.java
+++ ex2.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ex2 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a primeira nota: ");
+        double n1 = sc.nextDouble();
+
+        System.out.print("Digite a segunda nota: ");
+        double n2 = sc.nextDouble();
+
+        System.out.print("Digite a terceira nota: ");
+        double n3 = sc.nextDouble();
+
+        double media = (n1 + n2 + n3) / 3;
+        System.out.println("A média é: " + media);
+
+        sc.close();
     }
 }
